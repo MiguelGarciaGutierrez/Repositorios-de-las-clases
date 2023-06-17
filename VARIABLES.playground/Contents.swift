23b0cc5f212cd27bee96431a7388c36e2d7824c2
Mike <thelaastmigusevenationarmy@gmail.com>
2023-06-17 17:26:12 -0600

    DATATYPES

---
+++ VARIABLES.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+var nombre = "Miguel"
+var edad = 29
+var nombrecompleto = nombre + String(edad)
+var greeting = "Hello, playground"
+print(nombrecompleto)
+var numerogrande : Double = 45000000000000000000000.5555
+//Variables opcionales
+var variableOpcional : String?
+variableOpcional = nil
+print(variableOpcional)
